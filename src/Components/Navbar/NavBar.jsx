47ac9183a7aef9b426e3bfd616e5913cf5d8bacc
elisaul batista <elisaulbatista@pursuit.org>
2024-05-05 20:12:04 -0400

    setup the componetns and logo as svg now i will work on the routers

---
+++ src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+import Logo from "../Logo";
+function NavBar() {
+  return (
+    <div>
+      <Logo/>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/constants/images.js
@@ -0,0 +1,27 @@
+import alexZinder from "../assets/Speakers-img/Alex-Zinder.png";
+import dinaSaman from "../assets/Speakers-img/Dina-Saman.png";
+import gracyChen from "../assets/Speakers-img/Gracy-Chen.png";
+import helenHai from '../assets/Speakers-img/Helen-Hai.png'
+import jasonAllegrante from '../assets/Speakers-img/Jason-Allegrante.png'
+import jeanCharlesGaudechon from '../assets/Speakers-img/Jean-Charles-Gaudechon.png'
+import maxKordek from '../assets/Speakers-img/Max-Kordek.png'
+import mohammedAlblooshi from '../assets/Speakers-img/Mohammed-Alblooshi.png'
+import nenaDokuzov from '../assets/Speakers-img/Nena-Dokuzov.png'
+import nicolePurin from '../assets/Speakers-img/Reece-Merrick.png'
+import reeceMerrick from '../assets/Speakers-img/Reece-Merrick.png'
+import sunnyLu from "../assets/Speakers-img/Sunny-Lu.png";
+
+export default {
+    alexZinder,
+    dinaSaman,
+    gracyChen,
+    helenHai,
+    jasonAllegrante,
+    jeanCharlesGaudechon,
+    maxKordek,
+    mohammedAlblooshi,
+    nenaDokuzov,
+    nicolePurin,
+    reeceMerrick,
+    sunnyLu
+};
